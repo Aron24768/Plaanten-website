fbfc2c8278ef24d590e18372df047d37465148a6
wulliebeunhaas <24440@leerling.pieterzandt.nl>
2026-06-16 19:31:12 +0000

    kleur formulier opdracht

--- oefenen/onderwerp-3/form1v2.php
+++ oefenen/onderwerp-3/form1v2.php
@@ -1,22 +1,28 @@
 <?php
 
-if (isset($_GET["naam"])) {
-	$opgestuurde_naam = $_GET["naam"];
-} else {
-	$opgestuurde_naam = "hoe heet je?";
-}
+$opgestuurde_naam = $_GET["naam"] ?? "hoe heet je?";
+$kleur = $_GET["favcolor"] ?? "#000000";
 
-?><!DOCTYPE HTML>
+?>
+<!DOCTYPE HTML>
 <html>
 <head>
-<title>Formulier 1 versie 2</title>
+    <title>Formulier 1 versie 2</title>
 </head>
-<body>
-	<p>Hallo, <?=$opgestuurde_naam?></p>
-	<form>
-		<label>Naam: </label>
-		<input type="text" name="naam">
-		<input type="submit">
-	</form>
+<body style="color: <?=$kleur?>;">
+
+    <p>Hallo, <?=$opgestuurde_naam?></p>
+
+    <form>
+        <label>Naam: </label>
+        <input type="text" name="naam" value="<?=$opgestuurde_naam?>">
+	
+
+        <label for="favcolor">Select your favorite color:</label>
+        <input type="color" id="favcolor" name="favcolor" value="<?=$kleur?>">
+
+        <input type="submit" value="Versturen">
+    </form>
+
 </body>
 </html>
