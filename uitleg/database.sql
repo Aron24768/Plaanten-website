ba2d4b9ec70af30f9e384e4633e6d177eaa58298
Aron <24768@leerling.pieterzandt.nl>
2026-06-19 08:58:00 +0000

    we5ywsy54qw5y4

--- uitleg/database.sql
+++ uitleg/database.sql
@@ -8,12 +8,3 @@ CREATE TABLE IF NOT EXISTS accounts (
   email    varchar(100) NOT NULL UNIQUE
 );
  
-CREATE TABLE IF NOT EXISTS scores (
-  username varchar(20) NOT NULL PRIMARY KEY,
-  score    int         NOT NULL,
-
-  /* username in deze tabel verwijst naar de username in accounts: */
-  FOREIGN KEY (username) REFERENCES accounts(username)
-    ON DELETE CASCADE /* Als het account verwijderd wordt, verwijder ook de score */
-    ON UPDATE CASCADE /* Als de username in accounts verandert, verander die dan ook in de tabel scores */
-);
